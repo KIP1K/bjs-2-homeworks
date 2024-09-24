76837273f8900d15e3af52a368f43975fb7d0b80
KIP1K <141117821+KIP1K@users.noreply.github.com>
2024-09-24 09:17:47 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,44 +1,52 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let d = Math.pow(b, 2) - 4 * a * c;
+	let d = Math.pow(b, 2) - 4 * a * c;
 
-  if (d < 0) {
-    return [];
-  } else if (d === 0) {
-    let x = -b / (2 * a);
-    return [x];
-  } else {
-    let sqrtD = Math.sqrt(d);
-    let x1 = (-b + sqrtD) / (2 * a);
-    let x2 = (-b - sqrtD) / (2 * a);
-    return [x1, x2];
-  }
+	if (d < 0) {
+		return [];
+	} else if (d === 0) {
+		let x = -b / (2 * a);
+		return [x];
+	} else {
+		let sqrtD = Math.sqrt(d);
+		let x1 = (-b + sqrtD) / (2 * a);
+		let x2 = (-b - sqrtD) / (2 * a);
+		return [x1, x2];
+	}
+	console.log(solveEquation(1, -7, 12)); // [-3, 4]
+	console.log(solveEquation(1, -3, 2)); // [1]
+	console.log(solveEquation(1, 5, 6)); // []
 }
-
-console.log(solveEquation(1, -7, 12)); // [-3, 4]
-console.log(solveEquation(1, -3, 2)); // [1]
-console.log(solveEquation(1, 5, 6)); // []
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const monthlyRate = percent / 100 / 12;
+	const monthlyRate = percent / 100 / 12;
 
-  let principal = amount - contribution;
+	let principal = amount - contribution;
 
-  return {
-    principal,
-    monthlyPayment: principal * (monthlyRate * (1 + monthlyRate) ** countMonths / ((1 + monthlyRate)**countMonths - 1)),
-    totalPayment: countMonths * monthlyPayment
-  };
-}
+	const mortgage = {
+		principal,
+		monthlyPayment: principal * (monthlyRate * (1 + monthlyRate) ** countMonths / ((1 + monthlyRate) ** countMonths - 1))
+	};
 
-function roundNumber(num, decimalPlaces) {
-  decimalPlaces = Math.pow(10, decimalPlaces);
-  return Math.round(num * decimalPlaces) / decimalPlaces;
-}
+	mortgage.totalPayment = countMonths * mortgage.monthlyPayment;
 
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
-console.log(roundNumber(52749.53, 2)); 
+	let result1 = calculateTotalMortgage(10, 0, 50000, 12);
+	let roundedResult1 = roundNumber(result.totalPayment, 2);
+	console.log(roundedResult1);
 
-console.log(calculateTotalMortgage(10, 1000, 50000, 12)); 
-console.log(roundNumber(51694.54, 2)); 
+	let result2 = calculateTotalMortgage(11, 0, 60000, 12);
+	let roundedResult2 = roundNumber(result.totalPayment, 2);
+	console.log(roundedResult2);
+
+	let result3 = calculateTotalMortgage(16, 0, 40000, 12);
+	let roundedResult3 = roundNumber(result.totalPayment, 2);
+	console.log(roundedResult3);
+
+	let result4 = calculateTotalMortgage(10, 0, 90000, 18);
+	let roundedResult4 = roundNumber(result.totalPayment, 2);
+	console.log(roundedResult4);
+
+	let result5 = calculateTotalMortgage(8, 0, 550000, 24);
+	let roundedResult5 = roundNumber(result.totalPayment, 2);
+	console.log(roundedResult5);
